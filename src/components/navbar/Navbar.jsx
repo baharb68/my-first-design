29beb431fb2ad7a3cb3f77e5a9d9413a70169942
bahar <bagheri_bhr@yahoo.com>
2019-09-30 08:20:30 +0330

    creating navbar.jsx navbar.css and li component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Navbar.css'
+import Li from './Li'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
